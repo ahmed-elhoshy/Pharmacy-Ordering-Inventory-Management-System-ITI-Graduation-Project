8797b9f7618314028ae7c733ad3119c6b46ec0e5
Ahmed Baioumy <ahmedghoname00@gmail.com>
2025-06-20 23:18:12 +0300

    Updating pharmacy registration after adding userName column

--- PharmacySystem.ApplicationLayer/DTOs/Pharmacy/Register/PharmacyRegisterDto.cs
+++ PharmacySystem.ApplicationLayer/DTOs/Pharmacy/Register/PharmacyRegisterDto.cs
@@ -4,6 +4,11 @@ namespace PharmacySystem.ApplicationLayer.DTOs.Pharmacy.Register;
 
 public class PharmacyRegisterDto
 {
+    [Required]
+    //[StringLength(20, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 20 characters")]
+    //[RegularExpression("^[a-zA-Z0-9_]*$", ErrorMessage = "Username can only contain letters, numbers, and underscores")]
+    public string UserName { get; set; }
+
     [Required, MaxLength(100)]
     public string Name { get; set; }
 
